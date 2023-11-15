7fe6a154352335b883eab12e66b3e261c3bdc76d
shumniy <sergeishumilin1979@gmail.com>
2023-11-15 14:55:28 +0300

    if else

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "fff")
+{
+    Console.WriteLine("Ура, это же Мария");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
