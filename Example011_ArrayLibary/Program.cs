6c89705b9b12ec76361c28c73f6b158ec8e228ba
shumniy <sergeishumilin1979@gmail.com>
2023-11-18 21:55:10 +0300

    Решение  масивом 2

---
+++ Example011_ArrayLibary/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index=index+1;
+    }
+}
+
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+     while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position=position+1; // position++;
+    }
+}
+int [] array = new int [10];
+int Indexof(int [] collection, int find)
+    {
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+        {
+        if(collection[index] == find)
+            {   
+            position = index;
+            }
+        index=index+1;
+        }
+    return position;
+    }
+Console.Clear();
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
