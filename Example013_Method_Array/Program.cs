e4f8236635fe22871fdb7d85ed7dbbc29dfa86c6
shumniy <sergeishumilin1979@gmail.com>
2023-11-25 14:47:04 +0300

    Упорядочивание цифр масива с помощью метода

---
+++ Example013_Method_Array/Program.cs
@@ -0,0 +1,34 @@
+﻿// Упорядочивание цифр по порядку
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectSort (int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectSort(arr);
+
+PrintArray(arr);
