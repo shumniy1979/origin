48c6c4f139ccc52174d5add09fe2e38df13a958d
shumniy <sergeishumilin1979@gmail.com>
2023-12-04 11:08:57 +0300

    Преобразование стринг в чар и обработка

---
+++ Example014_string_char/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+// char[] chars = {'a', 'b', 'c', 'd'};
+Console.Write("Введите символы через пробел: ");
+char[] chars = Console.ReadLine()!.Split().Select(x => char.Parse(x)).ToArray();
+/*
+1. "a b 1 2 3 4 6 c d e"
+2. ["a", "b", "1", "2", "3", "4", "6", "c", "d", "e"]
+3. 'a', 'b', '1', '2', '3', '4', '6', 'c', 'd', 'e'
+4. ['a', 'b', '1', '2', '3', '4', '6', 'c', 'd', 'e']
+*/
+string result = string.Empty; // создание пустой строки
+// string result = "";
+foreach (char element in chars)
+{
+    result = result + element;
+}
+Console.WriteLine(result);
