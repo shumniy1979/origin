cc68d1142d27ae1498d76415f38557982429374b
shumniy <sergeishumilin1979@gmail.com>
2023-12-04 11:09:56 +0300

    Задание 2

---
+++ Example014_Zadanie2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите строку: ");
+string initial = Console.ReadLine()!;
+char[] chars = new char [initial.Length];
+// hello
+for (int i = 0; i < initial.Length; i++)
+{
+    chars[i] = initial[i];
+}
+    Console.WriteLine($"Результат: [{string.Join(" ", chars)}]");
