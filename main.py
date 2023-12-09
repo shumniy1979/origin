ed63bf3a5ef54ea6851bc877ef7d0830c208ef05
shumniy <sergeishumilin1979@gmail.com>
2023-12-09 13:07:41 +0300

    Фласк начало

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello, world!</h1>"
+
+
+@app.route("/info")
+def info():
+    return "<h1>GEEKBRAINS</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
